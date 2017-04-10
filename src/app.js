aff2782f2ff6f9a9f40acdd8d48d1d198a85afa1
Eric <eye1985@users.noreply.github.com>
2017-04-10 15:56:19 +0200

    - added missing folders

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const registerServiceWorker = () => {
+    if('serviceWorker' in navigator){
+        window.addEventListener('load',function(){
+            navigator.serviceWorker.register('../sw.js')
+                .then(registration => console.log(registration.scope,'Service worker registered'))
+                .catch(error => console.error(error, 'An error occured while registering service worker.'));
+        });
+    }else{
+        console.warn('This browser does not support service worker');
+    }
+};
+
+registerServiceWorker();
