b5a3ea91d15310097bcb40dee7378fd086590ab7
Eric <eye1985@users.noreply.github.com>
2017-04-10 16:09:50 +0200

    - Update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const registerServiceWorker = () => {
     if('serviceWorker' in navigator){
         window.addEventListener('load',function(){
-            navigator.serviceWorker.register('../sw.js')
+            navigator.serviceWorker.register('sw.js')
                 .then(registration => console.log(registration.scope,'Service worker registered'))
                 .catch(error => console.error(error, 'An error occured while registering service worker.'));
         });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.5';
+const CACHE_VERSION = '0.4';
 const urlsToCache = [
     '/',
     'index.html',
